8a07983a83bcc9ca809df980bc139c812f403514
Marian <ianlegaspi.marian@gmail.com>
2024-10-29 15:57:31 +0800

    Added if else statement for the valid number range, and initialize a value (0) for the number range like 1 - 10, 21- 30 up to 50, so everytime a user input a number within that range it will add 1 to said number range

--- number_range_counter.py
+++ number_range_counter.py
@@ -1,10 +1,30 @@
 #Initialize an empty list for numbers
 numbers = []
+#Initialize value for number ranges
+numbers_1_10 = 0
+numbers_11_20 = 0
+numbers_21_30 = 0
+numbers_31_40 = 0
+numbers_41_50 = 0
 #loop for the user input
 while True:
     #prompts user to enter a number between 1 - 50
     try:
         number = int(input(f"{'-'*50}\nEnter a number between 1 - 50 : "))
+        if 1 <= int(number) <= 10:
+            numbers_1_10 += 1
+        elif 21 <= int(number) <= 20:
+            numbers_11_20 += 1
+        elif 31 <= int(number) <= 30:
+            numbers_21_30 += 1
+        elif 31 <= int(number) <= 40:
+            numbers_31_40 += 1
+        elif 41 <= int(number) <= 50:
+            numbers_41_50 += 1 
+        #Exits loop when number is more than 50 or a non number / digit is entered
+        else:
+            print(f"{'-'*50}\nNumber out of range, exiting...")
+            break
     except ValueError:
         print(f"{'-'*50}\nInvalid input, exiting...")
         break
