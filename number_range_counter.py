5352ddf71e818ff22e731f661d13fdad1ced9045
Marian <ianlegaspi.marian@gmail.com>
2024-10-29 15:25:42 +0800

    Made a loop that ask for users to input numbers, the number range is not yet implemented.

--- number_range_counter.py
+++ number_range_counter.py
@@ -1,2 +1,11 @@
-#Initialize aan empty list for numbers
+#Initialize an empty list for numbers
 numbers = []
+#loop for the user input
+while True:
+    #prompts user to enter a number between 1 - 50
+    try:
+        number = int(input(f"{'-'*50}\nEnter a number between 1 - 50 : "))
+    except ValueError:
+        print(f"{'-'*50}\nInvalid input, exiting...")
+        break
+    
