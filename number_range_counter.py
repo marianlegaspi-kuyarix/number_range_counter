fef68e12e499f184d3ab34f49f9ea2743ef177ad
Marian <ianlegaspi.marian@gmail.com>
2024-10-29 17:13:18 +0800

    Adjusted the readability of the code

--- number_range_counter.py
+++ number_range_counter.py
@@ -1,16 +1,21 @@
 #Initialize an empty list for numbers
 numbers = []
+
 #Initialize value for number ranges
 numbers_1_10 = 0
 numbers_11_20 = 0
 numbers_21_30 = 0
 numbers_31_40 = 0
 numbers_41_50 = 0
+
 #loop for the user input
 while True:
+
     #prompts user to enter a number between 1 - 50
     try:
-        number = int(input(f"Enter a number between 1 - 50 : "))
+        number = int(input("Enter a number between 1 - 50 : "))
+        
+        #check if number is between 1 - 50
         if 1 <= int(number) <= 10:
             numbers_1_10 += 1
             numbers.append(int(number))
@@ -26,20 +31,24 @@ while True:
         elif 41 <= int(number) <= 50:
             numbers_41_50 += 1 
             numbers.append(int(number))
-        #Exits loop when number is more than 50 or a non number / digit is entered
+
+        #Exits loop when number is more than 50 
         else:
             print(f"{'_'*50}\nNumber out of range, exiting...")
             break
+    #Exits loop when input is invalid      
     except ValueError:
         print(f"{'_'*50}\nInvalid input, exiting...")
         break
+
 #Categorize numbers in different number ranges
 number_inside_1_10 = [number for number in numbers if 1 <= int(number) <= 10]  
 number_inside_11_20 = [number for number in numbers if 11 <= int(number) <= 20]  
 number_inside_21_30 = [number for number in numbers if 21 <= int(number) <= 30]  
 number_inside_31_40 = [number for number in numbers if 31 <= int(number) <= 40]  
-number_inside_41_50 = [number for number in numbers if 41 <= int(number) <= 50]    
-#Display how many times a number is entered within number ranges   
+number_inside_41_50 = [number for number in numbers if 41 <= int(number) <= 50]  
+
+#Display the count and the collection of numbers for each range  
 print(f"{'_'*50}\nNumbers count (1 - 10): {numbers_1_10}\nNumbers collection : {number_inside_1_10}")
 print(f"{'_'*50}\nNumbers count (11 - 20): {numbers_11_20}\nNumbers collection : {number_inside_11_20}")
 print(f"{'_'*50}\nNumbers count (21 - 30): {numbers_21_30}\nNumbers collection : {number_inside_21_30}")   
